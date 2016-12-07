0a58912df0901613820d9dddfb98aa3e053999e1
Manuel Naujoks <mnaujoks@gmail.com>
2016-12-07 22:18:03 +0100

    Now works in Edge (https://github.com/aurelia/framework/issues/668)

--- contact-manager/src/main.ts
+++ contact-manager/src/main.ts
@@ -2,12 +2,12 @@ import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
 
 //Configure Bluebird Promises.
-(<any>Promise).config({
-  longStackTraces: environment.debug,
-  warnings: {
-    wForgottenReturn: false
-  }
-});
+// (<any>Promise).config({
+//   longStackTraces: environment.debug,
+//   warnings: {
+//     wForgottenReturn: false
+//   }
+// });
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
