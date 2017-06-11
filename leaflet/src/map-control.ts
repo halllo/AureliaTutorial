fe6fea22ae13ff10e9c2a3b4d09bcde825868f59
Manuel Naujoks <mnaujoks@gmail.com>
2017-06-11 22:01:31 +0200

    moving marker, circular images, zoom to fit

--- leaflet/src/map-control.ts
+++ leaflet/src/map-control.ts
@@ -12,11 +12,12 @@ export class MapControl {
     this.map.on('click', e => this.setMarker(<L.LocationEvent>e));
     this.map.addLayer(L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', { 
       attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/feedback/" target="_blank">Improve this map</a></strong>',
-      minZoom: 4,
+      minZoom: 3,
+      maxZoom: 20,
       accessToken: environment.MAPBOX_AK,
     }));
     
-    var marker = L.marker([48.99759, 8.4105], { icon: new UserIcon({ iconUrl: 'https://njks.de/public/images/profile.png' }) })
+    let marker = L.marker([48.99759, 8.4105], { icon: new UserIcon({ iconUrl: 'https://pbs.twimg.com/profile_images/797050317871476736/VktgLGli_400x400.jpg' }) })
     marker.bindPopup("<b>Hello User!</b><br>I am a popup.").addTo(this.map);
     this.users.push(marker);
   }
@@ -24,27 +25,34 @@ export class MapControl {
   private setMarker(event: L.LocationEvent) {
     let location = event.latlng;
 
-    var newMarker = L.marker(location, { icon: new PointOfInterestIcon() });
+    let newMarker = L.marker(location, { icon: new PointOfInterestIcon() });
     newMarker.bindPopup("<b>Hello Point Of Interest</b><br>I am a popup at " + location.toString()).addTo(this.map);
     this.pointOfInterests.push(newMarker);
   }
 
   public zoomToFit() {
-    alert("zoom to fit");
-    // var group = L.featureGroup(this.users);
-    // this.map.fitBounds(group.getBounds());
+    let group = L.featureGroup(this.users.concat(this.pointOfInterests));
+    this.map.fitBounds(group.getBounds());
+  }
+
+  public moveTest() {
+    let user = this.users[0];
+    let oldPosition = user.getLatLng();
+    let newPosition = new L.LatLng(oldPosition.lat + 1, oldPosition.lng + 1, oldPosition.alt);
+    user.setLatLng(newPosition);
   }
 }
 
-var UserIcon = L.Icon.extend({
+let UserIcon = L.Icon.extend({
     options: {
       iconSize:     [90, 90],
       iconAnchor:   [45, 45],
-      popupAnchor:  [0, -45]
+      popupAnchor:  [0, -45],
+      className:    'img-circle'
     }
 });
 
-var PointOfInterestIcon = L.Icon.extend({
+let PointOfInterestIcon = L.Icon.extend({
     options: {
       iconUrl:      'images/poi.png',
       iconSize:     [25, 41],
--- leaflet/src/map.ts
+++ leaflet/src/map.ts
@@ -2,12 +2,9 @@ import {MapControl} from './map-control';
 
 export class Map {
 
-  public heading = 'Map Here';
   mapControl: MapControl;
 
   zoomToBounds() {
-    alert("custom element kann noch nicht getriggert werden...")
-    debugger;
     this.mapControl.zoomToFit();
   }
 
